0497719f4b0269016e1ba7f7a6878109de9bd197
vdelv <victor.delvigne@gmail.com>
2020-06-30 16:42:35 +0200

    Backup 30_06

--- eeg_preprocessing.py
+++ eeg_preprocessing.py
@@ -1,16 +1,35 @@
 from utils import *
 
+import matplotlib.pyplot as plt
+
 data_path = 'Dataset/'
-raw_eeg = np.load(data_path + 'raw_eeg.npy', allow_pickle=True)
+info_path = 'Information/'
+
+if not os.path.exists(data_path+'raw_eeg.npy'):
+    print('Dataset has not been generated from raw files.\nGenerating Dataset...')
+    exec(open("dataset_creation.py").read())
+else :
+    raw_eeg = np.load(data_path + 'raw_eeg.npy', allow_pickle=True)
 
 if not os.path.exists(data_path+'psd.npy'):
+    print('Spectral analysis has not been performed from EEG.\nGenerating PSD...')
     raw_eeg = mean_filtering(raw_eeg)
     spectral = psd_dataset(raw_eeg)
     np.save(data_path + 'psd', spectral)
-
 else:
     spectral = np.load(data_path+'psd.npy', allow_pickle=True).all()
 
-print('ffffffff')
+if not os.path.exists(data_path+'freq_band.npy'):
+    print('Frequency bands has not been computed from PSD.\nGenerating Freq bands...')
+    BandWith = freq_bands(spectral)
+    np.save(data_path+'freq_band', BandWith)
+else:
+    BandWith = np.load(data_path+'freq_band.npy')
 
-print(spectral)
+if not os.path.exists(data_path+'freq_img.npy'):
+    print('Frequency images have not been computed.\nGenerating Images...')
+    locs_3d = np.load(info_path+'ChanInfo.npy', allow_pickle=True).all()['position']
+    freq_img = band_image(BandWith, locs_3d)
+    np.save(data_path+'freq_img', freq_img)
+else:
+    np.load(data_path+'freq_img.npy')
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import numpy as np
+import math as m
 
 import os
 import mne
+
+from scipy.interpolate import griddata
+from sklearn.preprocessing import scale
 
 mne.set_log_level(verbose='CRITICAL')
 
@@ -131,6 +135,29 @@ def comp_acceleration(sig):
     return acc
 
 
+"""     ELECTRODES POSITION       """
+
+
+def electrodes_position(raw):
+    chan_dict = {}
+    pos = []
+    name = []
+    n_chan = len(raw.info['chs'])
+    for i in range(n_chan):
+        pos.append(raw.info['chs'][i]['loc'][0:3])
+        name.append(raw.info['chs'][i]['ch_name'])
+    chan_dict['position'] = np.asarray(pos)
+    chan_dict['chan_name'] = np.asarray(name)
+    return chan_dict
+
+
+def elec_proj(loc_3d):
+    locs_2d = []
+    for l in loc_3d:
+        locs_2d.append(azim_proj(l))
+    return np.asarray(locs_2d)
+
+
 """     EEG PREPROCESSING       """
 
 
@@ -170,9 +197,120 @@ def psd_dataset(raw_eeg):
                 Psd.append(psds)
                 Subject.append(s)
                 Task.append(k)
-    Spectral = {}
-    Spectral["frequency_sample"] = np.asarray(Freqs)
-    Spectral["power_spectral_density"] = np.asarray(Psd)
-    Spectral["Participant"] = np.asarray(Subject)
-    Spectral["Task"] = np.asarray(Task)
-    return Spectral
+    spectral = {}
+    spectral["frequency_sample"] = np.asarray(Freqs)
+    spectral["power_spectral_density"] = np.asarray(Psd)
+    spectral["Participant"] = np.asarray(Subject)
+    spectral["Task"] = np.asarray(Task)
+    return spectral
+
+
+def freq_bands(spectral_dict, theta_lim=[4, 8], alpha_lim=[8, 13], beta_lim=[13, 30]):
+    band_with = []
+    for epoch in range(spectral_dict['power_spectral_density'].shape[0]):
+        freqs = spectral_dict['frequency_sample'][epoch]
+        psd = spectral_dict['power_spectral_density'][epoch]
+
+        alpha_id = np.logical_and((freqs > alpha_lim[0]), (freqs < alpha_lim[1]))
+        beta_id = np.logical_and((freqs > beta_lim[0]), (freqs < beta_lim[1]))
+        theta_id = np.logical_and((freqs > theta_lim[0]), (freqs < theta_lim[1]))
+
+        alpha = psd[:, alpha_id].sum(axis=1)
+        beta = psd[:, beta_id].sum(axis=1)
+        theta = psd[:, theta_id].sum(axis=1)
+
+        band_with.append(np.asarray([theta, alpha, beta]))
+
+    band_with = np.asarray(band_with)
+    return band_with
+
+
+#################"      IMAGE GENERATION        #################
+
+def azim_proj(pos):
+    """
+    Computes the Azimuthal Equidistant Projection of input point in 3D Cartesian Coordinates.
+    Imagine a plane being placed against (tangent to) a globe. If
+    a light source inside the globe projects the graticule onto
+    the plane the result would be a planar, or azimuthal, map
+    projection.
+    :param pos: position in 3D Cartesian coordinates
+    :return: projected coordinates using Azimuthal Equidistant Projection
+    """
+    [r, elev, az] = cart2sph(pos[0], pos[1], pos[2])
+    return pol2cart(az, m.pi / 2 - elev)
+
+
+def cart2sph(x, y, z):
+    """
+    Transform Cartesian coordinates to spherical
+    :param x: X coordinate
+    :param y: Y coordinate
+    :param z: Z coordinate
+    :return: radius, elevation, azimuth
+    """
+    x2_y2 = x ** 2 + y ** 2
+    r = m.sqrt(x2_y2 + z ** 2)  # r
+    elev = m.atan2(z, m.sqrt(x2_y2))  # Elevation
+    az = m.atan2(y, x)  # Azimuth
+    return r, elev, az
+
+
+def pol2cart(theta, rho):
+    """
+    Transform polar coordinates to Cartesian
+    :param theta: angle value
+    :param rho: radius value
+    :return: X, Y
+    """
+    return rho * m.cos(theta), rho * m.sin(theta)
+
+
+def image_generation(feature_matrix, electrodes_loc, n_gridpoints):
+    n_electrodes = electrodes_loc.shape[0]  # number of electrodes
+    n_bands = feature_matrix.shape[1] // n_electrodes  # number of frequency bands considered in the feature matrix
+    n_samples = feature_matrix.shape[0]  # number of samples to consider in the feature matrix.
+
+    # Checking the dimension of the feature matrix
+    if feature_matrix.shape[1] % n_electrodes != 0:
+        print('The combination feature matrix - electrodes locations is not working.')
+    assert feature_matrix.shape[1] % n_electrodes == 0
+    new_feat = []
+
+    # Reshape a novel feature matrix with a list of array with shape [n_samples x n_electrodes] for each frequency band
+    for bands in range(n_bands):
+        new_feat.append(feature_matrix[:, bands * n_electrodes: (bands + 1) * n_electrodes])
+
+    # Creation of a meshgrid data interpolation
+    #   Creation of an empty grid
+    grid_x, grid_y = np.mgrid[
+                     np.min(electrodes_loc[:, 0]): np.max(electrodes_loc[:, 0]): n_gridpoints * 1j,  # along x_axis
+                     np.min(electrodes_loc[:, 1]): np.max(electrodes_loc[:, 1]): n_gridpoints * 1j  # along y_axis
+                     ]
+
+    interpolation_img = []
+    #   Interpolation
+    #       Creation of the empty interpolated feature matrix
+    for bands in range(n_bands):
+        interpolation_img.append(np.zeros([n_samples, n_gridpoints, n_gridpoints]))
+    #   Interpolation between the points
+    # print('Signals interpolations.')
+    for sample in range(n_samples):
+        for bands in range(n_bands):
+            interpolation_img[bands][sample, :, :] = griddata(electrodes_loc, new_feat[bands][sample, :],
+                                                              (grid_x, grid_y), method='cubic', fill_value=np.nan)
+    #   Normalization - replacing the nan values by interpolation
+    for bands in range(n_bands):
+        interpolation_img[bands][~np.isnan(interpolation_img[bands])] = scale(
+            interpolation_img[bands][~np.isnan(interpolation_img[bands])])
+        interpolation_img[bands] = np.nan_to_num(interpolation_img[bands])
+    return np.swapaxes(np.asarray(interpolation_img), 0, 1)  # swap axes to have [samples, colors, W, H]
+
+
+def band_image(frequency_band, electrodes_location, img_size=32):
+    locs_2d = elec_proj(electrodes_location)
+    frequency_band = frequency_band / np.min(frequency_band)
+    frequency_band = frequency_band.reshape((frequency_band.shape[0], -1))
+
+    images = image_generation(frequency_band, locs_2d, img_size)
+    return images
